3e08b2a9aaceeeb41c685d2fe191f5dbfd74efd7
xmfenbei <xmfenbei@outlook.com>
2018-04-28 15:22:17 +0800

    创建项目

---
+++ src/main/java/com/xmfenbei/projects/ProjectsApplication.java
@@ -0,0 +1,12 @@
+package com.xmfenbei.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsApplication.class, args);
+	}
+}
